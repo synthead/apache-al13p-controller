8ffcb708f6dc1b195c4bede331eb998322ccba77
Maxwell Pray <synthead@gmail.com>
2017-05-14 21:14:14 -0700

    Update hard-coded calibration data.

--- sensor.cpp
+++ sensor.cpp
@@ -4,7 +4,7 @@
 #define SENSOR_PIN A0
 
 // Max factory temperature 379F/193C.
-// 379F/193C is about 718 w/ 10K resistor from sensor to ground.
+// 379F/193C is about 738 w/ 10K resistor from sensor to ground.
 
 namespace Sensor {
   int raw;
@@ -12,7 +12,7 @@ namespace Sensor {
 
   // TODO: Read/write these values to/from EEPROM.
   // TODO: Allow calibration through menu.
-  float cal[2][2] = {{76, 20.4}, {718, 193}};
+  float cal[2][2] = {{76, 20.4}, {738, 193}};
   float m = (cal[1][1] - cal[0][1]) / (cal[1][0] - cal[0][0]);
   float b = cal[0][1] - m * cal[0][0];
 
